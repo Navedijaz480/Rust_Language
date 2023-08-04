9167fe17c2bb19723c3b438b13848a51eace99e2
NaveedIjazDev <108343564+Navedijaz480@users.noreply.github.com>
2023-08-04 11:45:03 +0500

    Add files via upload

--- src/modules/main.rs
+++ src/modules/main.rs
@@ -1,1 +1,8 @@
+use server::Server;
+mod server;
+fn main() {
+    // let server = server::Server::new("127.0.0.1".to_string());
+    let server = Server::new("127.0.0.1".to_string());
+    server.run();
+}
 
---
+++ src/modules/server.rs
@@ -0,0 +1,12 @@
+pub struct Server {
+    addr: String,
+}
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+   pub fn run(self) {
+        println!("Listening is on {}", self.addr);
+    }
+}
